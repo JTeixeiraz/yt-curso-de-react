87a6d215398ee1288f0ba900046f73f1d6105989
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2024-12-11 12:29:20 -0300

    commit inicial

---
+++ src/components/input.jsx
@@ -0,0 +1,13 @@
+function input(props){
+    return(
+        <input 
+        type={props.type}
+        placeholder={props.placeholder}
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+        value={props.value}
+        onChange={props.onChange}
+        />
+    )
+}
+
+export default input;
